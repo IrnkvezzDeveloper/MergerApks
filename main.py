6010aadea169f245933ae6773acc730d735bb630
Irnkvezz <irnkvezz.stdio@gmail.com>
2022-07-30 14:09:34 +0300

    master

--- main.py
+++ main.py
@@ -48,28 +48,46 @@ def copy_files(path1, path2):
 def generate_manifest():
     data = axml.parse('tmp/output/main.xml') # подгружаем изменяемый xml файл
     data2 = axml.parse('tmp/output/target.xml') # подгружаем второй xml файл
+
     root = data.getroot() # получаем root (tag <Manifest>)
     root2 = data2.getroot() # получаем root второго xml (tag <manifest>)
-    
-    activityes2 = root2.find('application').findall('activity')
-    activityes = root.find('application').findall('activity')
-    not_exists_act = []
+
+    activityes = root.find('application').findall('activity') #получаем список активити
+    activityes2 = root2.find('application').findall('activity') #получаем список активити
+
+    #перенос всех ненайденных активити
     for act in activityes2:
         act_name = act.attrib['{http://schemas.android.com/apk/res/android}name']
-        for act2 in activityes:
-            act_name2 = act2.attrib['{http://schemas.android.com/apk/res/android}name']
-            if act_name == act_name2:
-                print(act_name, act_name2)
-                add = False
-                break
-            else:
-                add = True
-        if add == True:
-            root.find('application').append(act2)
+        if act_name not in activityes:
+            print(f"Добавлено активити {act_name}")
+            root.find('application').append(act)
+
+    #перенос всех ненайденых сервисов
+
+    services = root.find('application').findall('service')
+    services2 = root2.find('application').findall('service')
+
+    for serv  in services2:
+        serv_name = serv.attrib['{http://schemas.android.com/apk/res/android}name']
+        if serv_name not in services:
+            print(f"Добавлен сервис {serv_name}")
+            root.find('application').append(serv)
+
+
+    #перенос всех ненайденных разрешений
+    perms = root.findall('uses-permission')
+    perms2 = root2.findall('uses-permission')
+
+    for perm in perms2:
+        perm_name = perm.attrib['{http://schemas.android.com/apk/res/android}name']
+        if perm_name not in perms:
+            print(f"Добавлено ненайденное разрешение {perm_name}")
+
+    # 
             
         
 
-    #выбор главного активити
+    # выбор главного активити
     activityes = root.find('application').findall('activity')
     activity_list = []
     idx = 0
@@ -78,8 +96,28 @@ def generate_manifest():
         activity_list.append(act_name)
         print(str(idx) + ". " +act_name)
         idx += 1
+
+    #удаляем все intent filter
+    for act in activityes:
+        if act.find('intent-filter'):
+            act.remove(act.find('intent-filter'))
+    
     main_act_index = input("Выберите главное активити >> ")
     choosed_main_act_name = activity_list[int(main_act_index)]
+    for i in activityes:
+        act_name = i.attrib['{http://schemas.android.com/apk/res/android}name']
+        if act_name == choosed_main_act_name:
+            #добавляем launcher intent filter
+            intent_filter = axml.Element('intent-filter')
+            action = axml.Element('action')
+            action.set('{http://schemas.android.com/apk/res/android}name', 'android.intent.action.MAIN')
+            category = axml.Element('category')
+            category.set('{http://schemas.android.com/apk/res/android}name', 'android.intent.category.LAUNCHER')
+            intent_filter.append(action)
+            intent_filter.append(category)
+            i.append(intent_filter)
+
+    data.write('output.xml')
     pass
 
 def build(target):
