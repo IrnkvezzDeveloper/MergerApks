c01783a95adb5c03fc0369dbecf090c38086a272
Irnkvezz <irnkvezz.stdio@gmail.com>
2022-07-30 11:41:51 +0300

    master

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import re
+import codecs
+import os 
+from os import listdir
+import shutil
+import subprocess
+import datetime
+
+apk_path1 = ""
+apk_path2 = ""
+
+def main():
+    apk_path1 = input("Введите путь до первого апк >> ")
+    apk_path2 = input("Введите путь до второго апк >> ")
+
+def detect_smali_folders():
+    pass
+
+def decompile_apk(apk_path):
+    pass
+
+def generate_manifest():
+    pass
+
+if __name__ == '__main__':
+    main()
